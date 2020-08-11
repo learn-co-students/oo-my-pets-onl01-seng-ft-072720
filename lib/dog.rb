b28b02fc30371a86d439a4033c6be8cdef978eab
Anthony Lepore <violacellonyc@gmail.com>
2020-08-11 18:53:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
   # code goes here
+  attr_accessor :owner, :mood 
+  attr_reader :name 
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    self.owner = owner
+    @mood = "nervous"
+    @@all.push(self)
+  end
+  
+def  self.all
+  @@all
+end
+  
+  
+  
 end
